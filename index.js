4a7803706944b61bbb5d3c249c3a4ea3352c5f46
Antonio <manentea@gmail.com>
2015-07-29 08:22:45 -0400

    added login logic

--- index.js
+++ index.js
@@ -4,29 +4,45 @@
   var app = angular.module('prox', []);
 
   app.controller('LoginController', function($http, $scope){
-    tab = 1;
 
     $scope.create = function(e){
       myBase.createUser({
-        email    : $scope.userName,
-        password : $scope.passWord
+        email    : $scope.user.userName,
+        password : $scope.user.passWord
       }, function(error, userData) {
         if (error) {
           console.log("Error creating user:", error);
         } else {
+          $scope.personForm2.$setPristine();
+          $scope.user = defaultForm;
+          $scope.personForm2.$destroy;
+          angular.element(personForm2).remove();
           console.log("Successfully created user account with uid:", userData.uid);
         }
       });
     };
 
-    $scope.login = function(e){
+    var defaultForm = {
+              userName: "",
+              passWord: ""
+    };
+
+    function loggedIn(authData){
+      alert("Hello " + authData.password.email)
+    };
+
+    $scope.login = function(){
       myBase.authWithPassword({
-        email    : $scope.userName,
-        password : $scope.passWord
+        email    : $scope.user.userName,
+        password : $scope.user.passWord
       }, function(error, authData) {
         if (error) {
           console.log("Login Failed!", error);
         } else {
+          $scope.personForm1.$setPristine();
+          $scope.user = defaultForm;
+          $scope.personForm1.$destroy;
+          angular.element(personForm1).remove();
           console.log("Authenticated successfully with payload:", authData);
         }
        }, {
