7f0be97e0f570f04333f5b2337ce367b13d4300f
Antonio <manentea@gmail.com>
2015-07-29 08:58:55 -0400

    added form styling, js to hide/show

--- index.js
+++ index.js
@@ -7,14 +7,14 @@
 
     $scope.create = function(e){
       myBase.createUser({
-        email    : $scope.user.userName,
-        password : $scope.user.passWord
+        email    : $scope.user.userName2,
+        password : $scope.user.passWord2
       }, function(error, userData) {
         if (error) {
           console.log("Error creating user:", error);
         } else {
           $scope.personForm2.$setPristine();
-          $scope.user = defaultForm;
+          $scope.user = defaultForm2;
           $scope.personForm2.$destroy;
           angular.element(personForm2).remove();
           console.log("Successfully created user account with uid:", userData.uid);
@@ -22,25 +22,29 @@
       });
     };
 
-    var defaultForm = {
-              userName: "",
-              passWord: ""
+    var defaultForm1 = {
+              userName1: "",
+              passWord1: ""
     };
 
+    var defaultForm2 = {
+              userName2: "",
+              passWord2: ""
+    };
     function loggedIn(authData){
       alert("Hello " + authData.password.email)
     };
 
     $scope.login = function(){
       myBase.authWithPassword({
-        email    : $scope.user.userName,
-        password : $scope.user.passWord
+        email    : $scope.user.userName1,
+        password : $scope.user.passWord1
       }, function(error, authData) {
         if (error) {
           console.log("Login Failed!", error);
         } else {
           $scope.personForm1.$setPristine();
-          $scope.user = defaultForm;
+          $scope.user = defaultForm1;
           $scope.personForm1.$destroy;
           angular.element(personForm1).remove();
           console.log("Authenticated successfully with payload:", authData);
